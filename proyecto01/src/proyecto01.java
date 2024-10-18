da9cef5138a12f4d34a72302f3339c091b5c41e5
DavMora <damora14@utpl.edu.ec>
2024-10-17 20:03:41 -0500

    primer commit

---
+++ proyecto01/src/proyecto01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author UTPL
+ */
+public class proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Recuerde usar la sentencia");
+    }
+         
+}
